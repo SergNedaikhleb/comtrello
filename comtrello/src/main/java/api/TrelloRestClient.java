033e162e0746a3fc4a539652eaca01bce646c38f
SergNedaikhleb <sergey.nedaikhleb@gmail.com>
2019-07-11 19:11:19 +0300

    complete project

--- comtrello/src/main/java/api/TrelloRestClient.java
+++ comtrello/src/main/java/api/TrelloRestClient.java
@@ -1,4 +1,42 @@
 package api;
 
+import api.interceptors.TrelloAuthInterceptor;
+import api.services.BoardsService;
+import api.services.CardsService;
+import api.services.ListsService;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.util.concurrent.TimeUnit;
+
 public class TrelloRestClient {
+
+    public static final String HOME_IO_BASE_URL = "https://api.trello.com/1/";
+
+
+    public BoardsService boardsService;
+    public ListsService listsService;
+    public CardsService cardsService;
+
+
+    public TrelloRestClient() {
+        OkHttpClient client = new OkHttpClient.Builder()
+                .connectTimeout(20, TimeUnit.SECONDS)
+                .writeTimeout(20, TimeUnit.SECONDS)
+                .readTimeout(20, TimeUnit.SECONDS)
+                .addInterceptor(new TrelloAuthInterceptor())
+                .build();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .client(client)
+                .baseUrl(HOME_IO_BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        boardsService = retrofit.create(BoardsService.class);
+        listsService = retrofit.create(ListsService.class);
+        cardsService = retrofit.create(CardsService.class);
+    }
+
 }
---
+++ comtrello/src/main/java/api/models/Board.java
@@ -0,0 +1,9 @@
+package api.models;
+
+public class Board {
+
+    public String id;
+    public String name;
+    public String desc;
+    public String url;
+}
